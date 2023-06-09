5775b9087fc4b988ba85c9bbb86e94c38e1b8b49
GlennWal <Glenn.vanderwal@student.rocva.nl>
2023-06-09 09:50:07 +0200

    Revert "Hoofdmenu(zonder nieuws banner!)"
    This reverts commit aebdb8f5a2eff01f5cf9cca524e1fd0114c4216d.

--- Php/Index.php
+++ Php/Index.php
@@ -10,37 +10,38 @@
     <title>Sky High</title>
 </head>
 <body>
+<div class="content">
 
-<div class="container">
+        <img src="SkyHighLogo.png" alt="SKYHIGH">
 
-    <div class="Logo">
-        <img src="SkyHighLogo.png" alt="SkyHighLogo">
+    <div class="container">
+
+        <header id="SKY HIGH">SKY HIGH</header>
+
+        <nav>
+            <a class="hover-underline-animation" href="#">HOME</a>
+            <a class="hover-underline-animation" href="#">FOTO'S</a>
+            <a class="hover-underline-animation" href="#">PLANNING</a>
+            <a class="hover-underline-animation" href="#">LEDENLIJST</a>
+            <a class="hover-underline-animation" href="#">CONTACT</a>
+        </nav>
+
+        <main>
+            <h1>HOME</h1>
+        </main>
     </div>
 
-    <div class="Registreren">
-        <a href="#" class="hover-underline-animation">REGISTREREN</a>
-        <a href="#" class="hover-underline-animation">INLOGGEN</a>
+        <div class="registreren">
+            <nav>
+                <a class="hover-underline-animation" href="#">REGISTREREN</a>
+                <a class="hover-underline-animation" href="#">INLOGGEN</a>
+            </nav>
+        </div>
     </div>
-
-    <header>Sky High</header>
-
-    <nav>
-        <a href="#" class="hover-underline-animation">HOME</a>
-        <a href="#" class="hover-underline-animation">FOTOS</a>
-        <a href="#" class="hover-underline-animation">PLANNING</a>
-        <a href="#" class="hover-underline-animation">LEDENLIJST</a>
-        <a href="#" class="hover-underline-animation">CONTACT</a>
-    </nav>
-
-    <main>
-        <h>HOME</h>
-    </main>
 
     <footer>
         <div id="ww_6bb00a1f5e810" v='1.3' loc='id' a='{"t":"horizontal","lang":"nl","sl_lpl":1,"ids":["wl7908"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"#FFFFFF00","cl_font":"#FFFFFF","cl_cloud":"#d4d4d4","cl_persp":"#2196F3","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'>More forecasts: <a href="https://oneweather.org/nl/amsterdam/30_days/" id="ww_6bb00a1f5e810_u" target="_blank">30 dagen weer</a></div><script async src="https://app1.weatherwidget.org/js/?id=ww_6bb00a1f5e810"></script>
     </footer>
-
-</div>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -48,7 +49,3 @@
 <script src="../Javascript/Javascript.js"></script>
 </body>
 </html>
-<!--class="hover-underline-animation"-->
-<!--src="SkyHighLogo.png" alt="SKYHIGH"-->
-<!--<div id="ww_6bb00a1f5e810" v='1.3' loc='id' a='{"t":"horizontal","lang":"nl","sl_lpl":1,"ids":["wl7908"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"#FFFFFF00","cl_font":"#FFFFFF","cl_cloud":"#d4d4d4","cl_persp":"#2196F3","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'>More forecasts: <a href="https://oneweather.org/nl/amsterdam/30_days/" id="ww_6bb00a1f5e810_u" target="_blank">30 dagen weer</a></div><script async src="https://app1.weatherwidget.org/js/?id=ww_6bb00a1f5e810"></script>-->
-
