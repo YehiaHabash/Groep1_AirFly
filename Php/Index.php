70c4a10d9faaa5ff73495cd0c588161fb4e993d3
GlennWal <Glenn.vanderwal@student.rocva.nl>
2023-06-12 09:11:29 +0200

    second commit

--- Php/Index.php
+++ Php/Index.php
@@ -40,7 +40,7 @@
     </div>
 
     <footer>
-        <div id="ww_6bb00a1f5e810" v='1.3' loc='id' a='{"t":"horizontal","lang":"nl","sl_lpl":1,"ids":["wl7908"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"#FFFFFF00","cl_font":"#FFFFFF","cl_cloud":"#d4d4d4","cl_persp":"#2196F3","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'>More forecasts: <a href="https://oneweather.org/nl/amsterdam/30_days/" id="ww_6bb00a1f5e810_u" target="_blank">30 dagen weer</a></div><script async src="https://app1.weatherwidget.org/js/?id=ww_6bb00a1f5e810"></script>
+        <div id="ww_77138ee80adf8" v='1.3' loc='id' a='{"t":"horizontal","lang":"nl","sl_lpl":1,"ids":["wl7908"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"#FFFFFF00","cl_font":"#000000","cl_cloud":"#d4d4d4","cl_persp":"#2196F3","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'>More forecasts: <a href="https://oneweather.org/nl/amsterdam/30_days/" id="ww_77138ee80adf8_u" target="_blank">30 dagen weer</a></div><script async src="https://app1.weatherwidget.org/js/?id=ww_77138ee80adf8"></script>
     </footer>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
