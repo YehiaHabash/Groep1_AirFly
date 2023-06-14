4a872c1bc654806fbfd28548fd1d1ea431ff4903
GlennWal <Glenn.vanderwal@student.rocva.nl>
2023-06-14 12:08:42 +0200

    nieuwe bestanden

--- Index.php
+++ Index.php
@@ -1,3 +1,9 @@
+<?php
+
+require_once 'database/conn.php';
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- database/conn.php
+++ database/conn.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "Skyhi";
+$dbname = "Skyhigh";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
