b17427193c097a314b0dcd2f9b3306f71ce647e8
SairaMae <118772523+SairaMae@users.noreply.github.com>
2023-06-15 09:06:59 +0200

    .

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
-<!--/--><?php
+<?php
 
-//require_once 'database/conn.php'; ?>
+require_once 'Php/database/conn.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +24,7 @@
 
     <div class="Registreren">
         <a href="#" class="hover-underline-animation">REGISTREREN</a>
-        <a href="Login.php" class="hover-underline-animation">INLOGGEN</a>
+        <a href="Php/inloggen.php" class="hover-underline-animation">INLOGGEN</a>
     </div>
 
     <header>Sky High</header>
@@ -40,6 +40,25 @@
     <main>
         <h>HOME</h>
     </main>
+
+    <?php
+    $sql = "SELECT * FROM users";
+    if ($result = $conn->query($sql)) {
+        while ($row = $result->fetch_row()) {
+            ?>
+            <p><?php echo $row[0]?></p>
+            <p><?php echo $row[1]?></p>
+            <p><?php echo $row[2]?></p>
+            <p><?php echo $row[3]?></p>
+            <p><?php echo $row[4]?></p>
+            <p><?php echo $row[5]?></p>
+
+
+            <?php
+        }
+    }
+
+    ?>
 
     <footer>
         <div id="ww_6bb00a1f5e810" v='1.3' loc='id' a='{"t":"horizontal","lang":"nl","sl_lpl":1,"ids":["wl7908"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"#FFFFFF00","cl_font":"#FFFFFF","cl_cloud":"#d4d4d4","cl_persp":"#2196F3","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'>More forecasts: <a href="https://oneweather.org/nl/amsterdam/30_days/" id="ww_6bb00a1f5e810_u" target="_blank">30 dagen weer</a></div><script async src="https://app1.weatherwidget.org/js/?id=ww_6bb00a1f5e810"></script>
