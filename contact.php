2f694db75233b59e0d46255546468857d8690b69
SairaMae <118772523+SairaMae@users.noreply.github.com>
2023-06-19 10:34:13 +0200

    a

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <script src="https://kit.fontawesome.com/87a9ed9bc2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/main.css">
     <meta charset="UTF-8">
     <meta name="description" content="Content">
@@ -30,6 +30,52 @@
         <a href="fotos.php" class="hover-underline-animation">FOTOS</a>
         <a href="planning.php" class="hover-underline-animation">PLANNING</a>
         <a href="ledenlijst.php" class="hover-underline-animation">LEDENLIJST</a>
+        <a href="contact.php" class="hover-underline-animation">CONTACT</a>
+    </nav>
 
+
+</div>
+
+<main>
+    <h>CONTACT</h>
+</main>
+<div class="contact">
+    <!--    <h2>CONTACT</h2>-->
+
+    <div class="row">
+        <div class="contact-col">
+
+            <img src="img/Telefoon.png">
+            <h2><br>Bel ons</h2>
+
+            <div class="layer">
+                <p>Telefonisch ondersteuning is beschikbaar <br> van 08:00 uur tot 17:00 uur.<br> <br> Bel ons gerust <br> +316123456789 </p>
+            </div>
+        </div>
+
+        <div class="contact-col">
+
+
+            <img src="img/email.png">
+            <h2><br>E-mail ons</h2>
+
+            <div class="layer2">
+                <p>Voor algemene vragen kunt u via e-mail contact met ons opnemen.</p>
+            </div>
+        </div>
+
+        <div class="contact-col">
+
+            <img src="img/location.png">
+            <h2><br>Bezoek ons</h2>
+
+            <div class="layer2">
+                <p>Adres, postcode, plaats, provincie.</p>
+            </div>
+
+        </div>
+
+    </div>
+</div>
 </body>
 </html>
