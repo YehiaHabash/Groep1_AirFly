35d464a5f654e8d8ab92512bc284ada404ec3eb8
GlennWal <Glenn.vanderwal@student.rocva.nl>
2023-06-19 11:43:16 +0200

    .

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-session_start();
-require 'database/conn.php';
+require_once 'database/conn.php';
 ?>
 
 <!DOCTYPE html>
--- registreren.php
+++ registreren.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require 'database/conn.php';
+require_once 'database/conn.php';
 ?>
 
 
